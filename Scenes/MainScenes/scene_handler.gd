89f64b585b1b5daadcffc565ccae950b423bc2b5
Tellusionist <Timothy.b.lee21@Gmail.com>
2025-09-20 20:37:35 -0700

    added basic UI

---
+++ Scenes/MainScenes/scene_handler.gd
@@ -0,0 +1,14 @@
+extends Node
+
+func _ready() -> void:
+	get_node("MainMenu/M/VB/NewGame").connect("pressed", on_new_game_pressed)
+	get_node("MainMenu/M/VB/Quit").connect("pressed", on_quit_pressed)
+
+func on_new_game_pressed() -> void:
+	# unload the main menu and instance the game scene handler
+	get_node("MainMenu").queue_free()
+	var game_scene = load("res://Scenes/MainScenes/game_scene.tscn").instantiate()
+	add_child(game_scene)
+
+func on_quit_pressed() ->  void:
+	get_tree().quit()
